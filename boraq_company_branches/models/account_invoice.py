1bc4fe868d66db1e7628d173f30c10de76066bed
Thierry <thierry.zock4@gmail.com>
2021-11-26 09:49:18 +0100

    [ADD] add module boraq_company_branches

---
+++ boraq_company_branches/models/account_invoice.py
@@ -0,0 +1,8 @@
+from odoo import api, fields, models, tools, _
+
+class AccountInvoice(models.Model):
+    _inherit = "account.move"
+
+    branch_id = fields.Many2one('company.branches', string='Branch', domain="[('company_id','=',company_id)]")
+    
+    
