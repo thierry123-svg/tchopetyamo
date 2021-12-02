bf0b68f2da7ec7b7e41c2fef4732428c00a51a3a
Thierry <thierry.zock4@gmail.com>
2021-12-02 16:29:30 +0100

    [IMP] correction errors

--- account_accountant/wizard/account_change_lock_date.py
+++ account_accountant/wizard/account_change_lock_date.py
@@ -17,7 +17,6 @@ class AccountChangeLockDate(models.TransientModel):
         default=lambda self: self.env.user.company_id.fiscalyear_lock_date,
         help='No users, including Advisers, can edit accounts prior to and inclusive of this date. Use it for fiscal year locking for example.')
 
-    @api.multi
     def change_lock_date(self):
         self.env.user.company_id.write({'period_lock_date': self.period_lock_date, 'fiscalyear_lock_date': self.fiscalyear_lock_date})
         return {'type': 'ir.actions.act_window_close'}
